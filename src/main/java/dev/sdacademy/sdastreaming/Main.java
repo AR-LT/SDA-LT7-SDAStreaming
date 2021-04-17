6fa4f5438586ff965fcb298f410bc9e6e6064b4c
RokasKaftanikas <rokas.kaft@gmail.com>
2021-04-17 13:42:45 +0300

    add author CRUD

--- src/main/java/dev/sdacademy/sdastreaming/Main.java
+++ src/main/java/dev/sdacademy/sdastreaming/Main.java
@@ -1,16 +1,19 @@
 package dev.sdacademy.sdastreaming;
 
+import dev.sdacademy.sdastreaming.entity.Author;
 import dev.sdacademy.sdastreaming.entity.Song;
+import dev.sdacademy.sdastreaming.repository.AuthorCRUDRepository;
 import dev.sdacademy.sdastreaming.repository.SongCRUDRepository;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class Main implements AutoCloseable {
 
     private static final String HOST = "localhost";  // 127.0.0.1 | localhost
     private static final String DB = "sdastreaming";
     private static final String USER = "root";
-    private static final String PASSWORD = "Dd%UB#s7x5yxDrhD";
+    private static final String PASSWORD = "gItara0130_";
 
     public static void main(String[] args) throws Exception {
         try (Main main = new Main()) {
@@ -20,11 +23,13 @@ public class Main implements AutoCloseable {
 
     private final Connection connection;
     private final SongCRUDRepository songRepository;
+    private final AuthorCRUDRepository authorRepository;
 
     public Main() {
         try {
             connection = DriverManager.getConnection("jdbc:mysql://" + HOST + ":3306/" + DB, USER, PASSWORD);
             songRepository = new SongCRUDRepository(connection);
+            authorRepository = new AuthorCRUDRepository(connection);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -42,10 +47,20 @@ public class Main implements AutoCloseable {
         newSong.setLength(100);
         newSong.setAuthorId(1);
         newSong.setGenreId(2);
-        songRepository.save(newSong);
+//        songRepository.save(newSong);
 
         songRepository.findAll().forEach(song -> {
             System.out.println(song);
+        });
+
+        Author newAuthor = new Author();
+        newAuthor.setId(7);
+        newAuthor.setName("James Bland");
+        newAuthor.setCreated(Date.valueOf(LocalDate.now()));
+//        authorRepository.save(newAuthor);
+//        authorRepository.delete(7);
+        authorRepository.findAll().forEach(author -> {
+            System.out.println(author);
         });
     }
 }
---
+++ src/main/java/dev/sdacademy/sdastreaming/repository/AuthorCRUDRepository.java
@@ -0,0 +1,83 @@
+package dev.sdacademy.sdastreaming.repository;
+
+import dev.sdacademy.sdastreaming.entity.Author;
+import dev.sdacademy.sdastreaming.exception.SDAStreamingException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorCRUDRepository {
+
+    private final Connection connection;
+
+    public AuthorCRUDRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    // CREATE
+    public void create(Author author) {
+        String sql = "INSERT INTO authors (name, created) VALUES (?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, author.getName());
+            stmt.setDate(2, author.getCreated());
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new SDAStreamingException(e);
+        }
+    }
+
+    // READ
+    public List<Author> findAll() {
+        List<Author> authors = new ArrayList<>();
+        try (Statement stmt = connection.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery("SELECT * FROM authors");
+            while (resultSet.next()) {
+                authors.add(toAuthor(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new SDAStreamingException(e);
+        }
+        return authors;
+    }
+
+    // UPDATE
+
+    public void update(Author author) {
+        String sql = "UPDATE authors SET name=?, created=? WHERE id=?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, author.getName());
+            stmt.setDate(2, author.getCreated());
+            stmt.setInt(3, author.getId());
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new SDAStreamingException(e);
+        }
+    }
+
+    // DELETE
+    public void delete(int id) {
+        try (PreparedStatement stmt = connection.prepareStatement("DELETE FROM authors WHERE id = ?")) {
+            stmt.setInt(1, id);
+            stmt.execute();
+        } catch (SQLException e) {
+            throw new SDAStreamingException(e);
+        }
+    }
+
+    public void save(Author author) {
+        if (author.getId() != null) {
+            update(author);
+        } else {
+            create(author);
+        }
+    }
+
+    private Author toAuthor(ResultSet rs) throws SQLException {
+        Author author = new Author();
+        author.setId(rs.getInt("id"));
+        author.setName(rs.getString("name"));
+        author.setCreated(rs.getDate("created"));
+        return author;
+    }
+}
